c6aa1dd1d69f38e3ae10045d5562d8350c90e3b1
Deepak Chowdry <work.deepakchowdry@gmail.com>
2025-04-02 16:14:26 +0530

    refined cases page

--- app/dashboard/newcase/page.tsx
+++ app/dashboard/newcase/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NewcasePage = () => {
     return (
-        <div className='flex items-center justify-center h-screen w-full'>
+        <div className='flex items-center justify-center py-12 min-h-screen w-full'>
             <CaseForm />
         </div>
     )
